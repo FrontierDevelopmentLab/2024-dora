a35673672aa0ada2c61d317babd34683ca00d0d8
raulramos <raulramos@google.com>
2024-12-15 14:43:59 -0500

    added scripts

---
+++ scripts/geedownloads_chipsets.sh
@@ -0,0 +1,16 @@
+CHIPSETS_FOLDER=/opt/earthdata/chipsets
+
+DATASET=esaworldcover-2021
+
+#DATASET=s1count-2017
+#PIXELS_LONLAT='--pixels_lonlat [32,32]'
+
+#DATASET=s1grdobs-202201-asc
+#AOI='--aoi lux'
+
+#DATASET=s2_13bands-2021
+#AOI='--aoi lux'
+
+SHUFFLE=--shuffle_order
+
+python geedownload_chipsets.py download --chipsets_folder $CHIPSETS_FOLDER --dataset $DATASET $AOI $PIXELS_LONLAT $SHUFFLE
